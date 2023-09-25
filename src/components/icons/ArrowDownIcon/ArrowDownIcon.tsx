e24e6d9fedbab62f549d883a90bf55380d60e2e9
StacyGru <stacy.gru@gmail.com>
2023-09-25 13:51:32 +0300

    Simplify color setting for icons using enum

--- src/components/icons/ArrowDownIcon/ArrowDownIcon.tsx
+++ src/components/icons/ArrowDownIcon/ArrowDownIcon.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import Icon from '../Icon';
 import {IconProps} from "components/Icons/Icon/Icon";
+import {Colors} from "utils/colors";
 
 const ArrowDownIcon: React.FC<IconProps> = (props) => {
 
 	const {icon: _, ...restProps} = props;
-
-	const primary: string = "#000000";
-	const secondary: string = "#AFADB5";
-	const accent: string = "#518581";
-
-	let localColor: string | undefined
-	localColor = props.color ? props.color : undefined
-
-	switch (props.color) {
-		case "primary":
-			localColor = primary
-			break
-		case "secondary":
-			localColor = secondary
-			break
-		case "accent":
-			localColor = accent
-			break
-	}
+	const color = Colors[props.color as keyof typeof Colors];
 
 	return (
 		<Icon icon={
-			<svg width="current" height="current" viewBox="0 0 24 24" fill={localColor}
+			<svg width="current" height="current" viewBox="0 0 24 24" fill={color}
 					 xmlns="http://www.w3.org/2000/svg" {...restProps}>
 				<path fillRule="evenodd" clipRule="evenodd"
 							d="M2.33563 8.74741L3.66436 7.25259L12 14.662L20.3356 7.25259L21.6644 8.74741L12 17.338L2.33563 8.74741Z"
--- src/components/icons/SideArrowIcon/SideArrowIcon.tsx
+++ src/components/icons/SideArrowIcon/SideArrowIcon.tsx
@@ -2,29 +2,12 @@ import React from 'react';
 import Icon from '../Icon';
 import styles from "./SideArrowIcon.module.scss";
 import {IconProps} from "components/Icons/Icon/Icon";
+import {Colors} from "utils/colors";
 
-const SideArrowIcon: React.FC<IconProps> = (props, rotate) => {
+const SideArrowIcon: React.FC<IconProps> = (props) => {
 
 	const {icon: _, ...restProps} = props;
-
-	const primary: string = "#000000";
-	const secondary: string = "#AFADB5";
-	const accent: string = "#518581";
-
-	let localColor: string | undefined
-	localColor = props.color ? props.color : undefined
-
-	switch (props.color) {
-		case "primary":
-			localColor = primary
-			break
-		case "secondary":
-			localColor = secondary
-			break
-		case "accent":
-			localColor = accent
-			break
-	}
+	const color = Colors[props.color as keyof typeof Colors];
 
 	return (
 		<Icon width={props.width} height={props.height} rotate="180deg" icon={
@@ -35,10 +18,10 @@ const SideArrowIcon: React.FC<IconProps> = (props, rotate) => {
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
 				className={props.rotate === "180deg" ? styles.svg_rotate : undefined}
-				{...props}
+				{...restProps}
 			>
 				<path d="M20.1201 26.56L11.4268 17.8667C10.4001 16.84 10.4001 15.16 11.4268 14.1333L20.1201 5.44"
-				      stroke={localColor}
+				      stroke={color}
 				      strokeWidth="1.5"
 				      strokeMiterlimit="10"
 				      strokeLinecap="round"
